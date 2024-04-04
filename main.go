8172806840f3349e68c63155ebd3516832fc2885
Indra Riksa Herlambang <indrariksa@gmail.com>
2024-04-04 22:15:12 +0700

    push pertama nih

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/indrariksa/ws-indra2024/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
-
-	"iteung/url"
+	"github.com/indrariksa/ws-indra2024/url"
 
 	"github.com/gofiber/fiber/v2"
 )
